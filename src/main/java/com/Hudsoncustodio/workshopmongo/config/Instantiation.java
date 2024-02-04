fcdf06cfe27daf0708914bdcdeeb220f69d73b07
Hudson Ferreira <141644371+Hudsoncustodio@users.noreply.github.com>
2024-02-04 18:43:30 -0300

    Acrescendo comentários aos posts

--- src/main/java/com/Hudsoncustodio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/Hudsoncustodio/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.Hudsoncustodio.workshopmongo.domain.Post;
 import com.Hudsoncustodio.workshopmongo.domain.User;
 import com.Hudsoncustodio.workshopmongo.dto.AuthorDTO;
+import com.Hudsoncustodio.workshopmongo.dto.CommentDTO;
 import com.Hudsoncustodio.workshopmongo.repository.PostRepository;
 import com.Hudsoncustodio.workshopmongo.repository.UserRepository;
 
@@ -40,7 +41,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("04/02/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("04/02/2024"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
-				
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("03/02/2024"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("04/02/2024"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótiomo dia!", sdf.parse("04/02/2024"), new AuthorDTO(alex));
+		
+		post1.getComment().addAll(Arrays.asList(c1, c2));
+		post2.getComment().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
--- src/main/java/com/Hudsoncustodio/workshopmongo/domain/Post.java
+++ src/main/java/com/Hudsoncustodio/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.Hudsoncustodio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.Hudsoncustodio.workshopmongo.dto.AuthorDTO;
+import com.Hudsoncustodio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {	
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;	
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comment = new ArrayList<>(); 
 	
 	public Post() {		
 	}	
@@ -75,6 +80,14 @@ public class Post implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
+	}	
 
 	@Override
 	public int hashCode() {
