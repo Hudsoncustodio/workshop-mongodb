6f6e30f7c57f62926e63a08abe9a6ac56317a00d
Hudson Ferreira <141644371+Hudsoncustodio@users.noreply.github.com>
2024-02-05 14:35:48 -0300

    Consulta com vários critérios

--- src/main/java/com/Hudsoncustodio/workshopmongo/resources/util/URL.java
+++ src/main/java/com/Hudsoncustodio/workshopmongo/resources/util/URL.java
@@ -1,15 +1,22 @@
 package com.Hudsoncustodio.workshopmongo.resources.util;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
-
 	public static String decodeParam(String text) {
+		return "";
+		}	
+
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		try {
-			return URLDecoder.decode(text,"UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			return " ";
-		}
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}		
 	}
 }
